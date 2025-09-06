e20443bcf73cf47a86685807c232cf97b0345dcd
지훈한 <hoony@jihunhan-ui-MacBookAir.local>
2025-09-06 15:07:41 +0900

    Fix CSS paths for GitHub Pages with basePath

---
+++ _next/static/chunks/33a7bb4f0e64f34f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/c84468449d292d86.js",
+  "static/chunks/turbopack-cf9190442fa62a3c.js"
+])
---
+++ _next/static/chunks/6696b69ef97bc538.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/c84468449d292d86.js",
+  "static/chunks/turbopack-c7e6fbf1b11044c7.js"
+])
