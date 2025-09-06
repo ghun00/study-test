f7d4e15e12f3f111c91f4e0c3a7e5e3fabceaeb2
지훈한 <hoony@jihunhan-ui-MacBookAir.local>
2025-09-06 15:18:13 +0900

    Deploy to GitHub Pages

---
+++ out/_next/static/build-1757139459539/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/33a7bb4f0e64f34f.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/study-test//_next/:path+",
+        "destination": "/study-test/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
